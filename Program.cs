1f9f4aca1568d40e0357c682986230d390c4547a
Carlos Ferrer <carlosvferrer@gmail.com>
2023-08-02 17:46:01 -0300

    CRUD no Entity Framework

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Blog.Data;
+using Blog.Models;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var context = new BlogDataContext())
+            {
+                // CREATE
+                // var tag = new Tag { Name = ".NET", Slug = "dotnet" };
+                // context.Tags.Add(tag);
+                // context.SaveChanges();
+
+                // var tag2 = new Tag { Name = "ASP.NET", Slug = "asp-net" };
+                // context.Tags.Add(tag2);
+                // context.SaveChanges();
+
+                // UPDATE
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                // tag.Name = ".NET";
+                // tag.Slug = "dotnet";
+
+                // context.Update(tag);
+                // context.SaveChanges();
+
+                //DELETE
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                // context.Tags.Remove(tag);
+                // context.SaveChanges();
+
+                //READ
+                var tags = context.Tags.ToList();
+
+                foreach(var tag in tags)
+                {
+                    Console.WriteLine(tag.Name);
+                }
+
+            }
+        }
+    }
+}
